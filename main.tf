0738a914ac22c664214d5510ca55b5ac5ce59c8f
Danylo Oliveira <danylo.oliveira73@gmail.com>
2023-11-08 13:27:27 -0300

    destroy cluster aws

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
+terraform {
+    required_version = ">=0.13.1"
+    required_providers {
+      aws = ">=3.54.0"
+      local = ">=2.1.0"
+    }
+    backend "s3" {
+      bucket = "tfmybucket"
+      key    = "terraform.tfstate"
+      region = "us-east-1"
+    }
+}
 
+provider "aws" {
+  region = "us-east-1"
+}
 module "new-vpc" {
   source = "./modules/vpc"
   prefix = var.prefix
